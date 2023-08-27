aaa10b7c4d0fe268dffcb6812115e8981b0309e7
anastasia_lykova <anastasia.7@mail.ru>
2023-08-28 00:39:01 +0500

    update and add product

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list,
+        'title': 'Главная'
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
@@ -11,4 +18,16 @@ def contacts(request):
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'You have new message from {name}({phone}): {message}')
-    return render(request, 'catalog/contacts.html')
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'catalog/contacts.html', context)
+
+
+def product(request, pk):
+    context = {
+        'object': Product.objects.get(pk=pk),
+        'title': 'Продукт'
+    }
+    return render(request, 'catalog/product.html', context)
+
