bb64ae10ebd94489cd950ccc14694d300fe1faf4
anastasia_lykova <anastasia.7@mail.ru>
2023-08-28 00:33:55 +0500

    update settings and urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from catalog.views import home, contacts
+from catalog.apps import CatalogConfig
+from catalog.views import home, contacts, product
 
-app_name = 'students_list'
+app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
+    path('product/<int:pk>/', product, name='product'),
 ]
